e0103a1e93f75647eda84d0ce244e1f81868b79a
Alexandre Macabies <web+git@zopieux.com>
2016-03-06 00:53:32 +0100

    meta: setup.py, add AUTHORS, add .gitignore, improve README, fix LICENSE

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
+# GitHub-Flavored markdown for Python
+
 This is an implementation of [GitHub-Flavored Markdown][gfm] written as an
 extension to the Python [Markdown][md] library. It aims for maximal
 compatibility with GitHub's rendering.
+
+
+## Supported features
+
+- Fenced code blocks
+- Literal line breaks
+- Tables
+- Hyperlink parsing (`http`, `https`, `ftp`, `email` and `www` subdomains)
+- Code highlighting (dummy, no actual syntactic coloration as-is)
+- Mixed-style lists with no separation
+- Links and images with whitespace
+- Strikethrough
+- Task lists
+
+
+## Unsupported features
+
+This implementation does not support all of GFM features.
+
+### Unsupported by design
+
+- Link to commits, issues, pull requests and user profiles: this is
+  application specific. Feel free to subclass the provided classes to
+  implement your own logic.
+
+### Unsupported, but planned
+
+- Horizontal rules
+- Emojis
+
+
+## License
+
+BSD-style. See [LICENSE](/LICENSE).
 
 [gfm]: http://github.github.com/github-flavored-markdown/
 [md]: http://packages.python.org/Markdown/
--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name='py-gfm',
     version='0.1.2',
-    description='An implementation of Github-Flavored Markdown written as an extension to the Python Markdown library.',
-    author='Dart Team',
+    description='An implementation of Github-Flavored Markdown written as an '
+                'extension to the Python Markdown library.',
+    author='Dart Team, Alexandre Macabies',
     author_email='misc@dartlang.org',
-    url='https://github.com/dart-lang/py-gfm',
-    download_url='https://github.com/dart-lang/py-gfm/tarball/0.1.2',
+    url='https://github.com/zopieux/py-gfm',
+    download_url='https://codeload.github.com/Zopieux/py-gfm/tar.gz/0.1.2',
     packages=find_packages(),
-    include_package_data = True,
-    install_requires = ['setuptools', 'markdown'],
+    include_package_data=True,
+    install_requires=['setuptools', 'markdown'],
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python'
+        'Topic :: Text Processing :: Markup',
     ]
 )
